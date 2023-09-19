ae1d9dc8287671bfc2dcce07bf8f0545b2cfc94a
Abdullah Mohammed <djangodeveloper6@gmail.com>
2023-09-20 01:13:40 +0300

    edit the defer attr in script tag in index.html page

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function initMap() {
         },
         mapTypeId: "roadmap",
         fullscreenControl: false, // Disable full-screen control
-        gestureHandling: "greedy" // Enable one-finger panning
+        gestureHandling: "greedy" // Enable one-finger panning, 
     });
 
     cities.forEach((city) => {
